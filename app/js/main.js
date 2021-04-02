65aedd1382ce5f29ec13f261ce89bd65f56ae947
unknown <anhout@gmail.com>
2021-04-02 20:52:05 +0300

    added filters

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,25 @@ $(function() {
         autoplay: true,
         autoplaySpeed: 2000
     });
+
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        onStart: function(data) {
+            $('.catalog-price__from').text(data.from);
+            $('.catalog-price__to').text(data.to);
+        },
+        onChange: function(data) {
+            $('.catalog-price__from').text(data.from);
+            $('.catalog-price__to').text(data.to);
+        },
+
+    });
+
+    $(".star").rateYo({
+        starWidth: "10px",
+        normalFill: "#d6d6d6",
+        ratedFill: "#ffcc00",
+    });
+
+
 });
