bfa6d13cbacb1a0f041766e49f9d0fae7b1b1d1a
unknown <anhout@gmail.com>
2021-04-18 19:09:59 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,9 @@
 $(function() {
 
+    $('.nav-user__menu').on('click', function() {
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+
     $('.product-tabs__top-item').on('click', function(e) {
         e.preventDefault();
         $('.product-tabs__top-item').removeClass('product-tabs__top-item--active');
